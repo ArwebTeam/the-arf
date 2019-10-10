fb3ccc6db91279e38e5b1c788049cfc1bb765dc8
Maciej Krüger <mkg20001@gmail.com>
2019-10-10 03:50:54 +0200

    feat: home page mock

--- public/vue/pages/home.vue
+++ public/vue/pages/home.vue
@@ -1,11 +1,66 @@
 <template>
   <div>
+    <!-- <center>
+      <br>
+      <span class="main-page-btn">
+        <a v-if="$user.p.admin && !isLive" href="/admin/pad">
+          <button type="button" class="btn btn-danger bmd-btn-fab">
+            <i class="fas fa-list"></i>
+          </button>
+        </a>
+        <a v-else-if="!isLive" href="/pad">
+          <button type="button" class="btn btn-danger bmd-btn-fab">
+            <i class="far fa-sticky-note"></i>
+          </button>
+        </a>
+        <a href="/tasks">
+          <button type="button" class="btn btn-success bmd-btn-fab">
+            <i class="fas fa-check-circle"></i>
+          </button>
+        </a>
+        <a href="/projects">
+          <button type="button" class="btn btn-purple bmd-btn-fab">
+            <i class="fas fa-project-diagram"></i>
+          </button>
+        </a>
+        <a v-if="!isLive" href="/protokolle">
+          <button type="button" class="btn btn-warning bmd-btn-fab" style="color: white">
+            <i class="fas fa-archive"></i>
+          </button>
+        </a>
+        <a href="/settings">
+          <button type="button" class="btn btn-info bmd-btn-fab">
+            <i class="fas fa-cog"></i>
+          </button>
+        </a>
+      </span>
+    </center> !-->
+
+    <br>
+    <h2>{{ $t('home.welcome') }}</h2>
+
     <br>
     <br>
 
-    <center>
-      <h1 style="font-size: 4em">{{ $t('home.welcome') }}</h1>
-    </center>
+    <h1>Topics</h1>
+    <br>
+
+    <div class="topic-list" v-for="topic in topics">
+      <i class="fa fa-3x icon-bulb fa-comment" :style="`background: ${str2col(topic.name)};`"></i>
+      <div class="topic-sub">
+        <h1>{{topic.name}}</h1>
+        <h2>{{topic.description}}</h2>
+      </div>
+    </div>
+
+    <div class="topic-list add-btn">
+      <i class="fa fa-3x icon-bulb fa-plus"></i>
+      <div class="topic-sub">
+        <h1>Add</h1>
+        <h2>Create a new topic</h2>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -16,7 +71,11 @@
   export default {
     name: 'home',
     data: () => ({
-      isLive: !module.hot
+      isLive: !module.hot,
+      topics: [{name: 'test', description: 'test'}, {name: 'test2', description: 'uffffffffff'}]
     }),
+    methods: {
+      str2col: require('string-to-color')
+    }
   }
 </script>
